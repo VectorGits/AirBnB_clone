28a7a7e8043a732556bf072392f9c28f214f0023
VectorGits <davidoluremi0@gmail.com>
2024-03-11 18:14:10 +0100

    Update HBNBCommand class with command descriptions

--- console.py
+++ console.py
@@ -1,21 +1,49 @@
+"""
+This module contains the HBNBCommand class, which is a command line interpreter
+for the AirBnB clone project. It uses the cmd module for the command line interface.
+"""
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-	"""Command line interpreter for AirBnB"""
+	"""
+	HBNBCommand class that contains the logic for the command line interpreter.
+	
+	Attributes:
+		prompt (str): The command line prompt string.
+	"""
+
 	prompt = '(hbnb) '
 
 	def do_quit(self, arg):
-		"""Quit command to exit the program"""
-		# self.exit(arg)
+		"""
+		Handles the 'quit' command, which exits the program.
+		
+		Args:
+			arg: The arguments passed to the 'quit' command. Not used.
+		
+		Returns:
+			True to signal the program to exit.
+		"""
 		return True
-	
+
 	def do_EOF(self, arg):
-		"""EOF command to exit the program"""
-		# self.exit(arg)
+		"""
+		Handles the 'EOF' command, which also exits the program.
+		
+		Args:
+			arg: The arguments passed to the 'EOF' command. Not used.
+		
+		Returns:
+			True to signal the program to exit.
+		"""
 		return True
-	
+
 	def emptyline(self):
-		"""Called when an empty line is entered in response to the prompt"""
+		"""
+		Handles the case where an empty line is entered in response to the prompt.
+		Does nothing.
+		"""
 		pass
 
 if __name__ == '__main__':
